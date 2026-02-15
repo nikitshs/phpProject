40b40f9829e193e79cb1a9822a63187293fbb89a
nikitshs <pnik244@gmail.com>
2026-02-15 19:12:51 +0500

    last step

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,4 +3,19 @@
         New page
     </x-slot>
     <p>this is a content of page</p>
+
+    @for($i = 0; $i < 10; $i++)
+        @php
+            $type = rand(1,3)
+        @endphp
+        <x-status :type=$type>
+            @if($type==1)
+                новый
+            @elseif($type==1)
+                подтвержденео
+            @else
+                отклонено
+            @endif
+        </x-status>
+    @endfor
 </x-main-layout>
